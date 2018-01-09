a2c5f1a1f9029dbfba07d73d4cbef4c9b701aeee
Daniela Puiu <dpuiu@jhu.edu>
2018-01-09 12:37:25 -0500

    ...

--- git.sh
+++ git.sh
@@ -1,5 +1,5 @@
 git clone https://github.com/dpuiu/minimus3.git
-git add *.pl
-git commit -m "A commit from my local computer"
-git push origin master
 
+git add *
+git commit -m "..."
+git push origin master
