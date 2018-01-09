cd5f8524b97230ebc2e869249948c16b27682365
Daniela Puiu <dpuiu@jhu.edu>
2018-01-09 12:36:29 -0500

    A commit from my local computer

--- git.sh
+++ git.sh
@@ -1,8 +1,5 @@
-git config --global user.name "Daniela Puiu"
-git config --global user.email "dpuiu@jhu.edu"
-git init .
+git clone https://github.com/dpuiu/minimus3.git
 git add *.pl
-git commit .
-git remote add origin "https://github.com/dpuiu/minimus3.git"
-git remote -v
+git commit -m "A commit from my local computer"
 git push origin master
+
